98d1a80170ebbcf027eab5bbbb1a82d1c39787a9
Remi Rampin <remi@rampin.org>
2025-02-27 10:55:25 -0500

    Add optional logging

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -19,28 +19,39 @@ type queryParser struct {
 	requiredLabels map[string]interface{}
 }
 
+func debugLog(msg string, args ...any) {
+	//log.Printf(msg, args...)
+}
+
+func (p *queryParser) outputByte(c byte) {
+	debugLog("output '%c'", c)
+	p.result.WriteByte(c)
+}
+
 func (p *queryParser) parse() error {
+	debugLog("parse %#v pos=%v", p.query, p.pos)
 	for {
 		p.consumeWhiteSpace()
+		debugLog("parse() after whitespace pos=%v", p.pos)
 		if p.pos >= len(p.query) {
 			return nil
 		}
 		c := p.query[p.pos]
 		switch {
 		case c == '{':
-			p.result.WriteByte(c)
+			p.outputByte(c)
 			p.pos += 1
 			if err := p.consumeSelectors(); err != nil {
 				return err
 			}
-		case (c == '[' || c == '(') || c == ')' ||
+		case (c == '[' || c == ']' || c == '(') || c == ')' ||
 			(c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_' ||
 			c == '=' || c == '<' || c == '>' || c == '!' || c == '|' ||
 			(c >= '0' && c <= '9'):
-			p.result.WriteByte(c)
+			p.outputByte(c)
 			p.pos += 1
 		case c == '"':
-			p.result.WriteByte(c)
+			p.outputByte(c)
 			p.pos += 1
 			if _, err := p.consumeString(); err != nil {
 				return err
@@ -53,26 +64,28 @@ func (p *queryParser) parse() error {
 
 func (p *queryParser) consumeWhiteSpace() {
 	for p.pos < len(p.query) && isWhiteSpace(p.query[p.pos]) {
-		p.result.WriteByte(p.query[p.pos])
+		p.outputByte(p.query[p.pos])
 		p.pos += 1
 	}
 }
 
 func (p *queryParser) consumeString() (string, error) {
+	debugLog("consumeString() pos=%v", p.pos)
 	start := p.pos - 1
 	for p.pos < len(p.query) {
 		c := p.query[p.pos]
-		p.result.WriteByte(c)
+		p.outputByte(c)
 		switch c {
 		case '"':
 			p.pos += 1
+			debugLog("consumeString() done pos=%v", p.pos)
 			return p.query[start:p.pos], nil
 		case '\\':
 			p.pos += 1
 			if p.pos >= len(p.query) {
 				return "", fmt.Errorf("missing closing string delimiter")
 			}
-			p.result.WriteByte(p.query[p.pos])
+			p.outputByte(p.query[p.pos])
 		}
 		p.pos += 1
 	}
@@ -80,25 +93,29 @@ func (p *queryParser) consumeString() (string, error) {
 }
 
 func (p *queryParser) consumeIdentifier() string {
+	debugLog("consumeIdentifier() pos=%v", p.pos)
 	start := p.pos
 	for p.pos < len(p.query) {
 		c := p.query[p.pos]
 		if (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_' || (p.pos > start && c >= '0' && c <= '9') {
-			p.result.WriteByte(c)
+			p.outputByte(c)
 			p.pos += 1
 		} else {
 			break
 		}
 	}
+	debugLog("consumeIdentifier() done pos=%v result=%#v", p.pos, p.query[start:p.pos])
 	return p.query[start:p.pos]
 }
 
 func (p *queryParser) consumeSelectors() error {
+	debugLog("consumeSelectors() pos=%v", p.pos)
 	missingLabels := maps.Clone(p.requiredLabels)
 	insertComma := false
 	for p.pos < len(p.query) {
 		c := p.query[p.pos]
 		if c == '}' {
+			debugLog("inserting %v missing labels", len(missingLabels))
 			for label := range missingLabels {
 				if insertComma {
 					p.result.WriteString(", ")
@@ -106,11 +123,12 @@ func (p *queryParser) consumeSelectors() error {
 				p.result.WriteString(label)
 				insertComma = true
 			}
-			p.result.WriteByte('}')
+			p.outputByte('}')
 			p.pos += 1
+			debugLog("consumeSelectors() done pos=%v", p.pos)
 			return nil
 		} else if c == ',' || c == ' ' {
-			p.result.WriteByte(c)
+			p.outputByte(c)
 			p.pos += 1
 		} else if (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_' {
 			var label strings.Builder
@@ -121,7 +139,7 @@ func (p *queryParser) consumeSelectors() error {
 			}
 			for p.pos < len(p.query) {
 				c := p.query[p.pos]
-				p.result.WriteByte(c)
+				p.outputByte(c)
 				p.pos += 1
 				if isWhiteSpace(c) {
 					continue
